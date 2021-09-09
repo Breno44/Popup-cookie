56c6937d641c97875d546ccb2e55021b0e5a03e8
Breno Henrique <bhfs.2908@gmail.com>
2021-09-09 19:20:55 -0300

    feat:finish

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -5,6 +5,11 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        text-transform: none;
+        outline: none;
+        border: none;
+        text-transform: capitalize;
+        transition: all 0.2s linear;
     }
 `;
 
